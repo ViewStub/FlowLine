31f99585484348c92c0ec6b3cc02b054f235dd78
wooou <122017720@qq.com>
2017-10-23 08:08:10 +0800

    更新  使用最简单的方式

--- app/src/main/java/com/viewstub/flowline/FlowLineView.java
+++ app/src/main/java/com/viewstub/flowline/FlowLineView.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.CornerPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
@@ -44,18 +45,32 @@ public class FlowLineView extends View {
     private Canvas mBitmapCanvas;
     private Paint mPaint = new Paint();
     private Paint mBitmapPaint = new Paint();
+    private final float roundWidth = 20;
+    int rA = 1; //暂时不能变
 
     private void init() {
+
+        CornerPathEffect cpe = new CornerPathEffect(20);
+
+        mPaint.setColor(Color.RED);
         mPaint.setStrokeWidth(5);
-        mPaint.setAntiAlias(true);
+        mPaint.setAntiAlias(false);
         mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setPathEffect(cpe);
+//        mPaint.setPathEffect(new DashPathEffect(new float[]{10,10},0));
+//        mPaint.setPathEffect(new DiscretePathEffect(3.0f, 5.0f));
+//        Path path = new Path();
+//        path.addCircle(0, 0, 3, Path.Direction.CCW);
+//        PathEffect pathEffect = new PathDashPathEffect(path, 12, 0, PathDashPathEffect.Style.ROTATE);
+//        mPaint.setPathEffect(pathEffect);
 
         mBitmapPaint.setStrokeWidth(3);
         mBitmapPaint.setAntiAlias(true);
         mBitmapPaint.setStyle(Paint.Style.STROKE);
+        mBitmapPaint.setPathEffect(cpe);
 
-        makeRoundPolygon(new RectF(50, 50, 450, 450), mPath);
-        makeRoundPolygon(new RectF(75, 75, 425, 425), mInnerPath);
+        makePolygon(new RectF(50, 50, 450, 450), mPath,6);
+        makePolygon(new RectF(75, 75, 425, 425), mInnerPath,6);
         mBitmap = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888);
         mBitmapCanvas = new Canvas(mBitmap);
     }
@@ -80,7 +95,6 @@ public class FlowLineView extends View {
         if (mBitmap != null) {
             canvas.drawBitmap(mBitmap, 0, 0, null);
         }
-        mPaint.setColor(Color.RED);
         canvas.drawPath(mInnerPath, mPaint);
 
 
@@ -89,65 +103,65 @@ public class FlowLineView extends View {
 
     }
 
-    final float roundWidth = 10;
-    float Gen3 = 1.7320508076f * roundWidth;
-    int rA = 1; //暂时不能变
+//    //手动计算(无法满足任意旋转)
+//    float Gen3 = 1.7320508076f * roundWidth;
 
-    private void makeRoundPolygon(RectF rect, Path path) {
+//    private void makeRoundPolygon(RectF rect, Path path) {
+//        float r = (rect.right - rect.left) / 2;
+//        float mX = (rect.right + rect.left) / 2;
+//        float my = (rect.top + rect.bottom) / 2;
+//        for (int i = 0; i < 6; i++) {
+//            float alpha = Double.valueOf(((2f / 6) * i - 0.5f*rA) * Math.PI).floatValue();//-0.5f 逆时针旋转90°
+//            float nextX = mX + Double.valueOf(r * Math.cos(alpha)).floatValue();
+//            float nextY = my + Double.valueOf(r * Math.sin(alpha)).floatValue();
+//            //TODO 非标准角度 ERROR
+//            int XR = (i  + rA -1) % 6;
+//            switch (XR) {
+//                case 0:
+//                    path.moveTo(nextX - Gen3, nextY + roundWidth);
+//                    path.quadTo(nextX, nextY, nextX + Gen3, nextY + roundWidth);
+//                    break;
+//                case 1:
+//                    path.lineTo(nextX - Gen3, nextY - roundWidth);
+//                    path.quadTo(nextX, nextY, nextX, nextY + roundWidth * 2);
+//                    break;
+//                case 2:
+//                    path.lineTo(nextX, nextY - roundWidth * 2);
+//                    path.quadTo(nextX, nextY, nextX - Gen3, nextY + roundWidth);
+//                    break;
+//                case 3:
+//                    path.lineTo(nextX + Gen3, nextY - roundWidth);
+//                    path.quadTo(nextX, nextY, nextX - Gen3, nextY - roundWidth);
+//                    break;
+//                case 4:
+//                    path.lineTo(nextX + Gen3, nextY + roundWidth);
+//                    path.quadTo(nextX, nextY, nextX, nextY - roundWidth * 2);
+//                    break;
+//                case 5:
+//                    path.lineTo(nextX, nextY + roundWidth * 2);
+//                    path.quadTo(nextX, nextY, nextX + Gen3, nextY - roundWidth);
+//                    break;
+//            }
+//        }
+//        path.close();
+//    }
+
+    private void makePolygon(RectF rect, Path path,int number) {
         float r = (rect.right - rect.left) / 2;
         float mX = (rect.right + rect.left) / 2;
         float my = (rect.top + rect.bottom) / 2;
-        for (int i = 0; i < 6; i++) {
-            float alpha = Double.valueOf(((2f / 6) * i - 0.5f*rA) * Math.PI).floatValue();//-0.5f 逆时针旋转90°
-            float nextX = mX + Double.valueOf(r * Math.cos(alpha)).floatValue();
-            float nextY = my + Double.valueOf(r * Math.sin(alpha)).floatValue();
-            //TODO 非标准角度 ERROR
-            int XR = (i  + rA -1) % 6;
-            switch (XR) {
-                case 0:
-                    path.moveTo(nextX - Gen3, nextY + roundWidth);
-                    path.quadTo(nextX, nextY, nextX + Gen3, nextY + roundWidth);
-                    break;
-                case 1:
-                    path.lineTo(nextX - Gen3, nextY - roundWidth);
-                    path.quadTo(nextX, nextY, nextX, nextY + roundWidth * 2);
-                    break;
-                case 2:
-                    path.lineTo(nextX, nextY - roundWidth * 2);
-                    path.quadTo(nextX, nextY, nextX - Gen3, nextY + roundWidth);
-                    break;
-                case 3:
-                    path.lineTo(nextX + Gen3, nextY - roundWidth);
-                    path.quadTo(nextX, nextY, nextX - Gen3, nextY - roundWidth);
-                    break;
-                case 4:
-                    path.lineTo(nextX + Gen3, nextY + roundWidth);
-                    path.quadTo(nextX, nextY, nextX, nextY - roundWidth * 2);
-                    break;
-                case 5:
-                    path.lineTo(nextX, nextY + roundWidth * 2);
-                    path.quadTo(nextX, nextY, nextX + Gen3, nextY - roundWidth);
-                    break;
-            }
-        }
-        path.close();
-    }
-
-    private void makePolygon(RectF rect, Path path) {
-        float r = (rect.right - rect.left) / 2;
-        float mX = (rect.right + rect.left) / 2;
-        float my = (rect.top + rect.bottom) / 2;
-        for (int i = 0; i <= 6; i++) {
-            float alpha = Double.valueOf(((2f / 6) * i - 0.5f*rA) * Math.PI).floatValue();
+        for (int i = 0; i < number; i++) {
+            float alpha = Double.valueOf(((2f / number) * i - 0.5f*rA) * Math.PI).floatValue();
             float nextX = mX + Double.valueOf(r * Math.cos(alpha)).floatValue();
             float nextY = my + Double.valueOf(r * Math.sin(alpha)).floatValue();
             if (i == 0) {
                 path.moveTo(nextX, nextY);
             } else {
                 path.lineTo(nextX, nextY);
-                path.moveTo(nextX, nextY);
+//                path.moveTo(nextX, nextY);
             }
         }
+        path.close();
     }
 
     public void start() {
